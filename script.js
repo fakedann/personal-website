d6b1e789b2b2de0a0b566674e4be321ecc26d6db
Daniel Escalona <daniel07escalona@gmail.com>
2021-12-27 13:16:15 -0500

    I believe these are tthe final touches

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const obj = document.getElementById("tw");
+
+obj.addEventListener("mouseover", function(){
+    this.style.color = "#1DA1F2";
+});
+
+obj.addEventListener("mouseout", function(){
+    this.style.color = "";
+});
+
+const obj2 = document.getElementById("im");
+
+obj2.addEventListener("mouseover", function(){
+    this.style.color = "#DBA506";
+});
+
+obj2.addEventListener("mouseout", function(){
+    this.style.color = "";
+});
+
